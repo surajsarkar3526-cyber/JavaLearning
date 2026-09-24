ca82f3a838c0a24eb73092f760960e347f605f62
Suraj Sarkar <surajsarkar3526@gmail.com>
2026-09-24 23:08:48 +0530

    New Update

--- src/Arrays/NextPermutation.java
+++ src/Arrays/NextPermutation.java
@@ -1,4 +1,38 @@
 package Arrays;
 
 public class NextPermutation {
+    public static void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.length - 1;
+
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1, nums.length - 1);
+    }
+    public static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    public static void reverse(int[] nums, int left, int right) {
+        while (left < right) {
+            swap(nums, left, right);
+            left++;
+            right--;
+        }
+    }
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 5, 4, 3};
+        nextPermutation(nums);
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
 }
